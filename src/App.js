6b543c89d08b2b9e266ff1c9cd583ae1b725aa6f
KubaRurak <69719179+KubaRurak@users.noreply.github.com>
2023-09-29 15:42:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world from new branch
+          Hello world from new branch, new stuff
         </p>
       </header>
     </div>
